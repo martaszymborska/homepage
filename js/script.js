2ad8519ec73eae090d7ee32b43e6a9bfe0c27404
martaszymborska <maszymborska@gmail.com>
2021-04-27 17:02:03 +0200

    test line added

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let buttonChange = document.querySelector(".js-buttonChange");
+let buttonHide = document.querySelector(".js-buttonHide");
+let main = document.querySelector(".main");
+let table = document.querySelector("table");
+
+
+main.classList.add("newBackground");
+
+buttonChange.addEventListener("click", () => {
+    main.classList.toggle("newBackground")
+        ();
+})
+
+<test>
+
+buttonHide.addEventListener("click", () => {
+    table.classList.toggle("hidden");
+
+    if (table.classList.contains("hidden")) {
+        buttonHide.innerText = "Pokaż tabelę";
+    }
+    else {
+        buttonHide.innerText = "Ukryj tabelę";
+
+    }
+
+});
